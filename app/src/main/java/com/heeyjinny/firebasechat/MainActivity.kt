dc3f25d07529dc8f3cf5cc950856ed053648a1f3
heeyjinny <heeyjinny@gmail.com>
2022-12-13 10:38:28 +0900

    Firebase Login

--- app/src/main/java/com/heeyjinny/firebasechat/MainActivity.kt
+++ app/src/main/java/com/heeyjinny/firebasechat/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.heeyjinny.firebasechat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.heeyjinny.firebasechat.databinding.ActivityMainBinding
+import com.heeyjinny.firebasechat.model.User
 
 /**  실시간 채팅 앱 만들기(파이어베이스-리얼타임 데이터베이스)  **/
 
@@ -42,9 +48,148 @@ import android.os.Bundle
 //채팅방생성, 생성된 채팅방 목록에 보여주는 클래스
 //방 아이디, 방 이름
 
+//7
+//로그인/회원가입 구현하기
+//액티비티 연결...
+//MainActivity.kt
 class MainActivity : AppCompatActivity() {
+
+    //뷰바인딩
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    //7-1
+    //데이터베이스 프로퍼티에 파이어베이스 생성
+    val database = Firebase.database
+
+    //7-2
+    //현재 액티비티에서 사용할 users 노드 연결
+    val userRef = database.getReference("users")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+        setContentView(binding.root)
+
+        //11
+        //로그인버튼 클릭시 signIn() 호출, 회원가입버튼 클릭 시 signUp() 호출
+        binding.btnSignin.setOnClickListener { signIn() }
+        binding.btnSignUp.setOnClickListener { signUp() }
+
+    }//onCreate
+
+    //8
+    //회원가입 버튼클릭 시 호출되는 함수생성
+    //signUp()
+    fun signUp(){
+        with(binding){
+            //8-1
+            //입력된 값 가져오기
+            val id = editId.text.toString()
+            val password = editPw.text.toString()
+            val name = editName.text.toString()
+
+            //8-2
+            //모두 값이 입력되어 있는지 검사***
+            if (id.isNotEmpty() && password.isNotEmpty() && name.isNotEmpty()){
+
+                //8-3
+                //화면에 모두 값이 입력되어 있다면...
+                //데이터베이스에 이미 아이디가 존재하는지 검사
+                userRef.child(id).get().addOnSuccessListener {
+
+                    if (it.exists()){
+                        Toast.makeText(baseContext, "아이디가 이미 존재합니다.", Toast.LENGTH_SHORT).show()
+                    }else{
+                        //8-4
+                        //데이터베이스에 아이디가 없다면
+                        //저장 후 자동으로 로그인
+                        //유저데이터클래스에 id,pw,name 전달
+                        val user = User(id, password, name)
+                        //8-5
+                        //데이터베이스 user노드안 id에 유저정보 추가...
+                        userRef.child(id).setValue(user)
+                        
+                        //8-6
+                        //회원가입 완료 알림 메시지 후
+                        //로그인...진행
+                        Toast.makeText(baseContext, "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show()
+                        signIn()
+                    }
+
+                }
+            }else{
+                //8-7
+                //입력창이 하나라도 비어있으면 메시지 출력
+                Toast.makeText(baseContext, "아이디, 비밀번호, 별명을 모두 입력해야 합니다.", Toast.LENGTH_SHORT).show()
+            }
+
+        }//with
+    }//signUp
+
+    //9
+    //로그인 버튼 클릭 시 호출되는 함수생성
+    //singIn()
+    fun signIn(){
+        with(binding){
+            //9-1
+            //화면에 입력된 아이디, 비밀번호 값 가져오기
+            val id = editId.text.toString()
+            val password = editPw.text.toString()
+
+            //9-2
+            //만약 아이디, 비밀번호 값이 모두 입력되었다면
+            if (id.isNotEmpty() && password.isNotEmpty()){
+                //9-3
+                //입력된 아이디로 User데이터 가져오기...
+                userRef.child(id).get().addOnSuccessListener {
+                    //9-4
+                    //id가 데이터 베이스에 있는지 확인
+                    if (it.exists()){
+                        //9-5
+                        //비밀번호 비교
+                        it.getValue(User::class.java)?.let {
+                            //9-6
+                            //비밀번호가 일치하다면
+                            if (it.password == password){
+                                //9-7
+                                //유저 아이디와 별명을 전달하면서
+                                //채팅방 목록 들어가기...(밑에 생성)
+                                goChatroomList(it.id, it.name)
+                            }else{
+                                //9-6-1
+                                //비밀번호 불일치
+                                Toast.makeText(baseContext, "비밀번호가 다릅니다.", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+
+                    }else{
+                        //9-4-1
+                        //아이디 없음
+                        Toast.makeText(baseContext, "아이디가 없습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }else{
+                //9-2-1
+                //아이디, 비밀번호 입력이 안되었다면
+                Toast.makeText(baseContext, "아이디, 비밀번호를 모두 입력해야 합니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }//signIn
+
+    //10
+    //로그인 성공 시 채팅방 목록으로 넘어가기
+    //사용자 정보를 파라미터로 전달
+    //아이디, 별명
+    fun goChatroomList(userId: String, userName: String){
+        //10-1
+        //채팅방 목록으로 넘어가는 인텐트 생성...
+        val intent = Intent(this, ChatListActivity::class.java)
+
+        //10-2
+        //방 생성 또는 입장시 사용
+        //인텐트 넘겨주면서 액티비티 시작
+        intent.putExtra("userId", userId)
+        intent.putExtra("userName", userName)
+        startActivity(intent)
+    }//goChatroomList
+
+}//MainActivity
